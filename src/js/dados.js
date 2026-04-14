4e264c9099223356e7eebb9d8bc1d465d6c63e15
Thiago-Natale <thiagohenrique.natale@gmail.com>
2026-04-14 15:57:32 -0300

    first commit

---
+++ src/js/dados.js
@@ -0,0 +1,13 @@
+const dados = {
+  nome: "Pamella Oliveira Argondizzi",
+  nome_upper: "",
+  cpf: "018.618.176-06",
+  dt_nascimento: "07/03/2000",
+  ra:"5156789",
+  email:"pamellaoliveira.argondizzi@edu.uniube.br",
+  validade: "12/06/2026"
+};
+
+dados.nome_upper = dados.nome.toUpperCase()
+
+export {dados}
