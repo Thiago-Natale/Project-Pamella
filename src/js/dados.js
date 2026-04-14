4223736550ac9657b8424151094908b3a82fb321
Thiago-Natale <thiagohenrique.natale@gmail.com>
2026-04-14 16:00:51 -0300

    ajuste

--- src/js/dados.js
+++ src/js/dados.js
@@ -3,7 +3,7 @@ const dados = {
   nome_upper: "",
   cpf: "018.618.176-06",
   dt_nascimento: "07/03/2000",
-  ra:"5156789",
+  ra:"5156782",
   email:"pamellaoliveira.argondizzi@edu.uniube.br",
   validade: "12/06/2026"
 };
